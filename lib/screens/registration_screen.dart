82651054c95daedbb95571ff6c7add8641bdea0e
wampet <wampet232@gmail.com>
2021-12-27 09:42:20 +0300

    added a comment

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,15 +1,33 @@
+// ignore_for_file: unnecessary_new
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 class RegistrationScreen extends StatefulWidget {
   RegistrationScreen({Key? key}) : super(key: key);
 
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
+//this is is living
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
 //We create some parameters
 //our form key
+// final _formKey = GlobalKey<FormState>();
+// //controllers
 
+// final TextEditingController firstNameController = new TextEditingController();
+// final TextEditingController lastNameController = new TextEditingController();
+// final TextEditingController emailController = new TextEditingController();
+// final TextEditingController passwordController = new TextEditingController();
+// final TextEditingController confirmPasswordController  = new TextEditingController();
+
+// final firstNameField = TextFormField(
+// autofocus: false,
+// controller: firstNameController,
+// keyboardType: TextInput.,
+
+// );
 
   @override
   Widget build(BuildContext context) {
